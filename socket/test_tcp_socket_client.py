ab96732e1ad85c1b3db99e66f9cb9552aade8756
gaohui965 <gaohui965@gmail.com>
2016-09-24 12:26:01 +0800

    Get server ip and port with command line.

--- socket/test_tcp_socket_client.py
+++ socket/test_tcp_socket_client.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
 
 from socket import *
+import sys
 
 HOST = '127.0.0.1'
 PORT = 8899
 BUFSIZE = 1024
-ADDR = (HOST, PORT)
+
+args_count = len(sys.argv) - 1
+if args_count == 0:
+    server_host = HOST
+    server_port = PORT
+elif args_count == 1:
+    server_host = sys.argv[1]
+    server_port = PORT
+elif args_count == 2:
+    server_host = sys.argv[1]
+    server_port = int(sys.argv[2])
+else:
+    print("Too must arguments!")
+    exit(1)
+
+server_addr = (server_host, server_port)
+print server_addr
 
 while True:
     # Create a socket and connect the sercer.
     tcp_client_sock = socket(AF_INET, SOCK_STREAM)
-    tcp_client_sock.connect(ADDR)
+    tcp_client_sock.connect(server_addr)
 
     # Send message to server.
     send_message_data = raw_input('> ')
